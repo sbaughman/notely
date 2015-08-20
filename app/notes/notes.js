3e8a50222dceb1775b17476822c6695a6f83d974
Seth Baughman <sbaughman1@me.com>
2015-08-20 10:46:55 -0400

    HTTP request GETs data from nevernote API

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var nevernoteBasePath = 'http://nevernote-1150.herokuapp.com/api/v1/';
+var apiKey = '$2a$10$mvUpe1bncgMjE0ciIl44VelGql7pcX1vCOc7Zif0Bmi1LFfdHutSa';
+
 angular.module('notely.notes', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +12,9 @@ angular.module('notely.notes', ['ngRoute'])
   });
 }])
 
-.controller('NotesController', [function() {
-
+.controller('NotesController', ['$scope', '$http', function($scope, $http) {
+  $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)
+    .success(function(notesData) {
+      $scope.notes = notesData;
+    });
 }]);
